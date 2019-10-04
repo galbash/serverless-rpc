1f7cafa385c1cd3bd74b4eb3366e9a57c7a64538
Gal Bashan <gal@epsagon.com>
2019-10-04 15:30:17 +0300

    docs(plugin-example): adding python-python example

---
+++ plugin/examples/simple-server-client/server/server/handler.py
@@ -0,0 +1,63 @@
+"""
+An example for a Lambda handler using the serverless-rpc framework
+"""
+from tutorial import Calculator
+from tutorial.ttypes import InvalidOperation, Operation
+from shared.ttypes import SharedStruct
+from serverless_thrift.server.TLambdaServer import TLambdaServer
+
+
+class CalculatorHandler:
+    """
+    The handler that implements the Calculator service.
+    """
+    def __init__(self):
+        self.log = {}
+
+    def ping(self):
+        print('ping()')
+
+    def add(self, n1, n2):
+        print('add(%d,%d)' % (n1, n2))
+        return n1 + n2
+
+    def calculate(self, logid, work):
+        print('calculate(%d, %r)' % (logid, work))
+
+        if work.op == Operation.ADD:
+            val = work.num1 + work.num2
+        elif work.op == Operation.SUBTRACT:
+            val = work.num1 - work.num2
+        elif work.op == Operation.MULTIPLY:
+            val = work.num1 * work.num2
+        elif work.op == Operation.DIVIDE:
+            if work.num2 == 0:
+                x = InvalidOperation()
+                x.whatOp = work.op
+                x.why = 'Cannot divide by 0'
+                raise x
+            val = work.num1 / work.num2
+        else:
+            x = InvalidOperation()
+            x.whatOp = work.op
+            x.why = 'Invalid operation'
+            raise x
+
+        log = SharedStruct()
+        log.key = logid
+        log.value = '%d' % (val)
+        self.log[logid] = log
+
+        return val
+
+    def getStruct(self, key):
+        print('getStruct(%d)' % (key))
+        return self.log.get(key, 'Key not found - stored on a different Lambda instance')
+
+    def zip(self):
+        print('zip()')
+
+
+handler = CalculatorHandler()
+processor = Calculator.Processor(handler)
+server = TLambdaServer(processor)
