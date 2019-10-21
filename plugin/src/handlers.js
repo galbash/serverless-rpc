32c5310392c08dc7b787c5cc41b4d8c11fc61062
Gal Bashan <gal@epsagon.com>
2019-10-21 21:50:33 +0300

    feat(plugin-node): adding node support for the plugin

--- plugin/src/handlers.js
+++ plugin/src/handlers.js
@@ -2,6 +2,16 @@ import { join } from 'path';
 import _ from 'lodash';
 
 const WRAPPER_CODE = {
+  node: `
+const serverlessThrift = require('serverless-thrift');
+const { HANDLER: rpcInternalHandler } = require('../HANDLER_PATH.js');
+const SERVICE = require('../SERVICE_PATH');
+
+module.exports.EXPORTED_SERVER = serverlessThrift.createLambdaServer(
+    SERVICE,
+    rpcInternalHandler
+);
+`,
   python: `
 from HANDLER_PATH import HANDLER as rpc_internal_handler
 from SERVICE_PATH import SERVICE
@@ -19,25 +29,67 @@ const FILE_NAME_BY_LANG_GENERATORS = {
 
 export const SUPPORTED_LANGUAGES = Object.keys(WRAPPER_CODE);
 
-function getPathByLanguage(relativePath, language) {
-  const path = (
-    language === 'python' ?
-      relativePath.replace(/\//g, '.').replace(/\\/g, '.') :
-      relativePath
-  );
-  return path;
+/**
+ * fixes a file path to be imported, based on a language.
+ * @param path The path of the file to be imported
+ * @param language The function language
+ * @return {String} The fixed path
+ */
+function fixImportPathByLanguage(path, language) {
+  switch (language) {
+  case 'python':
+    return path.replace(/\//g, '.').replace(/\\/g, '.');
+
+  default:
+    return path;
+  }
 }
 
+/**
+ * Calculates the path in which the RPC service is defined, based on the language
+ * @param outputPath The path the RPC files were generated to
+ * @param serviceDirName The name of the service directory
+ * @param serviceName The name of the service
+ * @param language The function language
+ * @return {String} The path to the service, ready to be imported (language appropriate)
+ */
+function getServicePathByLanguage(outputPath, serviceDirName, serviceName, language) {
+  let path;
+  switch (language) {
+  case 'python':
+    path = join(outputPath, serviceDirName);
+    break;
+
+  case 'node':
+    path = join(outputPath, serviceName);
+    break;
+
+  default:
+    path = join(outputPath, serviceDirName);
+    break;
+  }
+
+  return fixImportPathByLanguage(path, language);
+}
+
+/**
+ * Generates RPC wrapper code for a single function
+ * @param func The sls function
+ * @param config The service's RPC config
+ * @return {String} The generated wrapper code
+ */
 export function generateWrapperCode(func, config) {
-  const relativePath = getPathByLanguage(func.relativePath, func.language);
+  const relativePath = fixImportPathByLanguage(func.relativePath, func.language);
   const { exportedHandlerName = func.rpcHandlerObject } = func;
   const [serviceDirName, serviceName] = _.last(
     func.rpcConfig.service.replace(/\\/g, '/').split('/')
   ).split('.');
-  const servicePath = getPathByLanguage(join(
+  const servicePath = getServicePathByLanguage(
     func.rpcConfig.outputPath || config.outputPath,
-    serviceDirName
-  ), func.language);
+    serviceDirName,
+    serviceName,
+    func.language
+  );
   return WRAPPER_CODE[func.language]
     .replace(/HANDLER_PATH/g, relativePath)
     .replace(/HANDLER/g, func.rpcHandlerObject)
--- plugin/src/thrift_utils.js
+++ plugin/src/thrift_utils.js
@@ -1,7 +1,42 @@
 import fs from 'fs-extra';
 import cp from 'child_process';
 
-export default function generateThrift(includeDirs, outDir, target, serviceDef) {
+/**
+ * Converts a Lambda runtime name to a Thrift language name
+ * @param runtime The Lambda runtime name
+ * @return {String} The language name
+ */
+function runtimeToThriftLanguage(runtime) {
+  if (!runtime) {
+    return null;
+  }
+
+  if (runtime.startsWith('python')) {
+    return 'py';
+  }
+
+  if (runtime.startsWith('node')) {
+    return 'js';
+  }
+
+  return null;
+}
+
+/**
+ * Generates thrift code from IDL
+ * @param includeDirs directories to search .thrift files in
+ * @param outDir The directory to output the generated files to
+ * @param runtime The Lambda runtime
+ * @param options The options to use for generation
+ * @param serviceDef the path for the service to generate
+ */
+export default function generateThrift(includeDirs, outDir, runtime, serviceDef, options = '') {
+  const language = runtimeToThriftLanguage(runtime);
+  let usedOptions = options;
+  if (language === 'js') {
+    usedOptions = `node,${options}`;
+  }
+  const target = usedOptions && usedOptions !== '' ? `${language}:${usedOptions}` : language;
   fs.mkdirpSync(outDir);
   cp.spawnSync(
     'thrift',
