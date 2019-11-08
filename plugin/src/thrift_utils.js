e45b8ce2b093c47794d025897a4261324ba982d4
Gal Bashan <gal@epsagon.com>
2019-11-08 16:39:29 +0200

    feat(plugin-thrift): report errors on thrift generation failures (#52)

--- plugin/src/thrift_utils.js
+++ plugin/src/thrift_utils.js
@@ -2,6 +2,52 @@ import fs from 'fs-extra';
 import cp from 'child_process';
 
 /**
+ * Base class for Thrift errors
+ */
+export class ThriftError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = this.constructor.name;
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+/**
+ * An error that is raised when the thrift compiler binary is not found.
+ */
+export class ThriftNotFoundError extends ThriftError {
+  /**
+   * @param path The path thrift was searched for
+   */
+  constructor(path) {
+    super(`Could not locate the thrift compiler. searched for: "${path}". Please make sure it is installed.`);
+    this.path = path;
+  }
+}
+
+/**
+ * An error that is raised when a thrift service generation fails.
+ */
+export class ThriftGenerationFailedError extends ThriftError {
+  constructor(errorCode, stderr) {
+    super(`Thrift generation failed. error code: ${errorCode}. stderr: ${stderr}`);
+    this.errorCode = errorCode;
+    this.stderr = stderr;
+  }
+}
+
+/**
+ * An error that is raised when a thrift service generation is interrupted.
+ */
+export class ThriftGenerationInterruptedError extends ThriftError {
+  constructor(signal, stderr) {
+    super(`Thrift generation interrupted. signal code: ${signal}. stderr: ${stderr}`);
+    this.signal = signal;
+    this.stderr = stderr;
+  }
+}
+
+/**
  * Converts a Lambda runtime name to a Thrift language name. If already given a thrift
  * Language, returns it unchanged.
  * @param runtime The Lambda runtime name
@@ -39,8 +85,22 @@ export default function generateThrift(includeDirs, outDir, runtime, serviceDef,
   }
   const target = usedOptions && usedOptions !== '' ? `${language}:${usedOptions}` : language;
   fs.mkdirpSync(outDir);
-  cp.spawnSync(
+  const result = cp.spawnSync(
     'thrift',
     ['-I', includeDirs.join(' '), '-r', '-out', outDir, '--gen', target, serviceDef]
   );
+
+  if (result.error) {
+    if (result.error.errno === 'ENOENT') {
+      throw new ThriftNotFoundError(result.error.path);
+    }
+    throw result.error;
+  }
+
+  if (result.status !== 0) {
+    if (!result.status && result.signal) {
+      throw new ThriftGenerationInterruptedError(result.signal, result.stderr);
+    }
+    throw new ThriftGenerationFailedError(result.status, result.stderr);
+  }
 }
